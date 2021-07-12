a6f5744f5c9a80c59579299c5fd5d7eff6282680
mbrianna1170 <brianna.miller09@yahoo.com>
2021-07-11 22:39:35 -0500

    added prompt questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,41 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const basicQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please provide a detailed description of your project:'
+        },
+        {
+            type: 'input',
+            name: 'intallation',
+            message: 'Please provide information on the installation process:'
+        },
+        {
+            type: 'checkbox',
+            name: 'license',
+            message: 'What license did you use? (Select one from the list)',
+            choices: ['MIT', 'GNU AGPL', 'Mozilla', 'None']
+
+        }    
+
+    ]);
+};  
+
+basicQuestions().then(answers => console.log(answers));
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(filename, data) {}
+  
 
 // TODO: Create a function to initialize app
 function init() {}
